5c842c969b1647bcef58b8959a2166974111de0e
Chepelev Anton <chepelash@gmail.com>
2019-03-24 22:20:22 +0300

    Added testbench

--- tb/priority_encoder_tb.sv
+++ tb/priority_encoder_tb.sv
@@ -44,10 +44,66 @@ task automatic apply_rst;
 endtask
 
 
+task automatic wait_for_data;
+  @( posedge clk );
+  @( posedge clk );
+  @( posedge clk );
+endtask
+
+
+task automatic rand_numb_test;
+
+  bit [WIDTH-1:0] check_val_left;
+  bit [WIDTH-1:0] check_val_right;
+  bit [WIDTH-1:0] check_val_input;
+  
+  for( int j = 0; j < 5; j++ )
+      begin
+        check_val_input  = $urandom_range(0, 2**WIDTH - 1);
+        data_i          <= check_val_input;        
+        
+        for( int i = 0; i < WIDTH; i++ )
+          begin
+            if( check_val_input[i] )
+              begin
+                check_val_right[i] = 1'b1;
+                break;
+              end
+          end
+        for( int i = WIDTH - 1; i >= 0; i-- )
+          begin
+            if ( check_val_input[i] )
+              begin
+                check_val_left[i] = 1'b1;
+                break;
+              end
+          end
+          
+        wait_for_data();
+        
+        if( ( data_left_o == check_val_left ) && ( data_right_o == check_val_right ) )
+          begin
+            $display("OK! Input = %8b; Output left = %8b; Output right = %8b;", 
+                      data_i, data_left_o, data_right_o);
+          end
+        else
+          begin
+            $display("Fail! Input = %8b; Output left = %8b; Output right = %8b;",
+                      data_i, data_left_o, data_right_o);
+            $stop();
+          end
+        check_val_left  = '0;
+        check_val_right = '0;
+      end
+
+endtask
+
+
  always
   begin
     clk_gen();
   end
+ 
 
 always
   begin
@@ -56,24 +112,12 @@ always
    
     $display("Starting!\n");
     
-    apply_rst();
+    apply_rst();       
+    rand_numb_test();
     
-//    bit [WIDTH-1:0] check_val;
-    
-    for( int i = 0; i < 5; i++ )
-      begin
-        data_i <= $urandom_range(0, 2**WIDTH - 1);
-        @( posedge clk );             
-      end
-    
-    @( posedge clk );
-    @( posedge clk );
-    @( posedge clk );
-    @( posedge clk );
+    $display("Everything is OK!");
     $stop();
   end
-
-
 
 
 endmodule
